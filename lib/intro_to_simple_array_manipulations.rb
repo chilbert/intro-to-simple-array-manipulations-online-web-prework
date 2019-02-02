20b90fb20e92047ce8d32b02db743681803abf93
Chase Hilbert <chase@heyspacetime.com>
2019-02-02 23:59:19 +0000

    Automatically backed up by Learn

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,8 @@
+def using_push(countries_in_western_africa, next_country)
+  updated_array = countries_in_western_africa.push(next_country)
+end
+
+def using_unshift (neighborhoods_in_northwest_brooklyn, two)
+
+
+end
