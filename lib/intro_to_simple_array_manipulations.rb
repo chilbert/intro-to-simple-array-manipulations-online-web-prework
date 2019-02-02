6a65b284ec3a01b733fa5b052607912e5b01844f
Chase Hilbert <chase@heyspacetime.com>
2019-02-02 23:59:36 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -3,6 +3,5 @@ def using_push(countries_in_western_africa, next_country)
 end
 
 def using_unshift (neighborhoods_in_northwest_brooklyn, two)
-
-
+neighborhoods_in_northwest_brooklyn.unshift(two)
 end
