1058bbad3600d104df28dae68176ef3a777ef3e3
Chase Hilbert <chase@heyspacetime.com>
2019-02-03 00:09:35 +0000

    Done.

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,47 @@
+def using_push(countries_in_western_africa, next_country)
+  updated_array = countries_in_western_africa.push(next_country)
+end
+
+def using_unshift (neighborhoods_in_northwest_brooklyn, two)
+  neighborhoods_in_northwest_brooklyn.unshift(two)
+end
+
+def using_pop(pop)
+  pop.pop
+end
+
+def pop_with_args(one)
+  one.pop(2)
+end
+
+def using_shift(one)
+  one.shift
+end
+
+def shift_with_args(one)
+  one.shift(2)
+end
+
+def using_concat(one, two)
+  one.concat(two)
+end
+
+def using_insert(one, two)
+  one.insert(4, two)
+end
+
+def using_uniq(one)
+  one.uniq
+end
+
+def using_flatten(one)
+  one.flatten
+end
+
+def using_delete (one, two)
+  one.delete(two)
+end
+
+def using_delete_at(one, two)
+  one.delete_at(two)
+end
